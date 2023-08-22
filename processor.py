90f07051ef02d69378790626a16bd3222b80580a
Mateusz <smetmateusz@gmail.com>
2023-08-22 13:00:03 +0200

    Beutifuller

--- processor.py
+++ processor.py
@@ -3,7 +3,7 @@ import csv
 output_message = ""
 with open('netbox_data.csv', 'w', newline='') as csvfile:
     data_writer = csv.writer(csvfile, delimiter=',')
-    data_writer.writerow(["Device", "Name", "Type", "Enabled"])
+    data_writer.writerow(["device", "name", "type", "enabled"])
     with open("ansible_data") as input_file:
         for line in input_file:
             if line[0] != '}':
@@ -18,12 +18,12 @@ with open('netbox_data.csv', 'w', newline='') as csvfile:
                     switch_name = output[10]
                     port_name = output[12].strip("\\\"\\n\"")
 
-                    data_writer.writerow([f"{device_name}, {device_port}, 1000BASE-T (1GE), True"])
+                    data_writer.writerow([device_name, device_port, "1000BASE-T (1GE)", "True"])
                 else:
                     device_name = output[1].strip("[]")
                     device_port = output[3].split('=')[1].strip(')')
                     internal_connection = "True"
 
-                    data_writer.writerow([f"{device_name}, {device_port}, Virtual, True"])
+                    data_writer.writerow([device_name, device_port, "Virtual", "True"])
 
                 output_message = ""
